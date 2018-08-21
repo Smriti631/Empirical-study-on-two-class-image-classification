bc7102bd7bb9ecff9ac9cf06df5b87a35b4b88e3
Smriti631 <42540013+Smriti631@users.noreply.github.com>
2018-08-21 14:59:37 +0530

    Add files via upload

---
+++ indoor_outdoor.py
@@ -0,0 +1,138 @@
+import cv2                 # working with, mainly resizing, images
+import numpy as np         # dealing with arrays
+import os                  # dealing with directories
+from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm      # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
+
+import tensorflow as tf
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+TRAIN_DIR = '/home/smriti/Desktop/project_python/CNN/train'
+TEST_DIR = '/home/smriti/Desktop/project_python/CNN/test'
+IMG_SIZE = 50
+LR = 1e-3
+
+MODEL_NAME = 'in_vs_out_classify-{}-{}.model'.format(LR, '2conv-basic') # just so we remember which saved model is which, sizes must match
+
+def label_img(img):
+    word_label = img.split('.')[-3]
+    # conversion to one-hot array [cat,dog]
+    #                            [much cat, no dog]
+    if word_label == 'indoor': return [1,0]
+    #                             [no cat, very doggo]
+    elif word_label == 'outdoor': return [0,1]
+    
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
+
+
+def process_test_data():
+    testing_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR,img)
+        img_num = img.split('.')[0]
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        testing_data.append([np.array(img), img_num])
+        
+    shuffle(testing_data)
+    np.save('test_data.npy', testing_data)
+    return testing_data
+
+    
+train_data = create_train_data()   
+process_test_data() 
+    
+train_data = np.load('train_data.npy')   
+ 
+
+import tensorflow as tf
+tf.reset_default_graph()
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 128, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+
+if os.path.exists('/home/smriti/Desktop/project_python/CNN/{}.meta'.format(MODEL_NAME)):
+    model.load(MODEL_NAME)
+    print('model loaded!')
+
+train = train_data[:-200]
+test = train_data[-200:]
+
+X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y = [i[1] for i in train]
+
+test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y = [i[1] for i in test]
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}), 
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+model.save(MODEL_NAME)
+import matplotlib.pyplot as plt
+import numpy as np  
+# if you need to create the data:
+#test_data = process_test_data()
+# if you already have some saved:
+
+test_data = np.load('test2_data.npy')
+
+fig=plt.figure()
+
+for num,data in enumerate(test_data[:11]):
+    # cat: [1,0]
+    # dog: [0,1]
+    
+    img_num = data[1]
+    img_data = data[0]
+    
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    #model_out = model.predict([data])[0]
+    model_out = model.predict([data])[0]
+    
+    if np.argmax(model_out) == 1: str_label='outdoor'
+    else: str_label='indoor'
+        
+    y.imshow(orig,cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
